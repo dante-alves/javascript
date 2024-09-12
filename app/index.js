c974f453d86780456c1d75d891e8a0dfc7805f25
Dante Alves <dantealves8@gmail.com>
2024-09-12 11:07:13 -0300

    teste

---
+++ app/index.js
@@ -0,0 +1,11 @@
+// () => {} é uma arrow function
+const start = () => {
+    let count = 0
+    while (count < 10) {
+        console.log(count)
+        count ++
+    }
+}
+
+start() 
+console.log ('Olá mundo')
